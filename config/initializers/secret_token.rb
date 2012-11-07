aa71853b0c7e508ca083bbce8b933071de2da020
allen niu <kingplesk@gmail.com>
2012-11-08 00:59:39 +0800

    remove all files add readme file

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = 'dbbbaa8384bcb565c53ef878dc9325598d642b70802ee19bef13fdd93725549f168e2c924fa66f47b333826468650a7e4a20e90182b0f0f7fd7771d0a91971be'
